95d75ccc7615ae56e2aee902e974737a7eaeb807
Vadim Homchik <homchik@gmail.com>
2024-07-11 16:19:30 +0200

    Inherit hast interfaces for layout

--- src/types.ts
+++ src/types.ts
@@ -1,46 +1,37 @@
+import {Root as HastRoot, Node as HastNode, Element as HastElement } from "hast";
+
 export interface Segment {
-  id: string;
-  text: string;
-  tags?: Tags;
+  id: string
+  text: string
+  tags?: Tags
 }
 
 export type TagAttributes = {
-  [key: string]: string;
+  [key: string]: string
 }
 
 export type Tags = {
-  [key: string]: TagAttributes;
+  [key: string]: TagAttributes
 }
 
-export interface Layout {
-  type: "root";
-  children: LayoutNode[];
+export interface LayoutRoot extends HastRoot {}
+
+export interface LayoutSegment extends HastNode {
+  type: "segment"
+  id: string
 }
 
-export interface LayoutSegment {
-  type: "segment";
-  id: string;
-}
-
-export interface LayoutElement {
-  value?: string;
-  type: string;
-  tagName: string;
-  children: LayoutNode[];
-  properties?: any
-}
-
-export type LayoutNode = LayoutElement | LayoutSegment;
+export interface LayoutElement extends HastElement {}
 
 export type Context = any;
 
 export interface Document {
-  segments: Segment[];
-  layout: Layout;
+  segments: Segment[]
+  layout: LayoutRoot
   metadata?: { [key: string]: any }
 }
 
 export interface Processor {
-  parse(res: string, ctx?: Context): Document;
-  stringify(doc: Document, ctx?: Context): string;
+  parse(res: string, ctx?: Context): Document
+  stringify(doc: Document, ctx?: Context): string
 }
